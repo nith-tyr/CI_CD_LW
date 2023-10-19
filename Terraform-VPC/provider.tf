3ad29338aa73ac3de6e3876ed5ba45cbce924f7f
nith-tyr <nitheesh.bp@igsindia.net>
2023-10-19 20:59:04 +0530

    Adding Terraform and github actin files

---
+++ Terraform-VPC/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.16.1"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-tfvars-backend-s3"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
